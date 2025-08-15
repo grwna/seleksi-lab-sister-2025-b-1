c4a7bbd98c751a316a595b673d33e54ecaa5b623
grwna <13523035@std.stei.itb.ac.id>
2025-08-15 18:32:23 +0700

    TST - add api testing script

--- 7-Roses are red, kerjain TST, or sister-chan won't be your fiancé/API/test/test-request.py
+++ 7-Roses are red, kerjain TST, or sister-chan won't be your fiancé/API/test/test-request.py
@@ -1,7 +1,8 @@
 import requests
 import json
 
-token = "YOUR_TOKEN_HERE"
+token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJncnduYSIsImVtYWlsIjoicmVzcGVjdHNlcmVuZGlwaXR5QGdtYWlsLmNvbSIsImlhdCI6MTc1NTI1NTI5NiwiZXhwIjoxNzU1MzQxNjk2fQ.vEZgT99STPSvaDMz1y5pchrSU7dDKayV9P6hOJXdxuw"
+
 text_to_summarize = "This is a long text that I want to summarize using the API without a browser."
 
 headers = {
@@ -18,6 +19,16 @@ response = requests.post("http://localhost:3000/api/summarize", headers=headers,
 if response.status_code == 200:
     print("Success!")
     print(response.json())
+else:
+    print("Error:")
+    print(response.status_code, response.text)
+
+response = requests.get("http://localhost:3000/api/history", headers=headers, data=json.dumps(data))
+
+if response.status_code == 200:
+    print("Success!")
+    history = response.json()
+    print(json.dumps(history, indent=2))
 else:
     print("Error:")
     print(response.status_code, response.text)
