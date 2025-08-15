ca0cbcdb720e669474e23c3818bf16d1112191bd
grwna <13523035@std.stei.itb.ac.id>
2025-08-15 17:53:49 +0700

    TST - add test script

---
+++ 7-Roses are red, kerjain TST, or sister-chan won't be your fiancé/API/test/test-request.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+token = "YOUR_TOKEN_HERE"
+text_to_summarize = "This is a long text that I want to summarize using the API without a browser."
+
+headers = {
+    "Authorization": f"Bearer {token}",
+    "Content-Type": "application/json"
+}
+
+data = {
+    "text": text_to_summarize
+}
+
+response = requests.post("http://localhost:3000/api/summarize", headers=headers, data=json.dumps(data))
+
+if response.status_code == 200:
+    print("Success!")
+    print(response.json())
+else:
+    print("Error:")
+    print(response.status_code, response.text)
